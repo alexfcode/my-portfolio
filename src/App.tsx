9811da564a607a8b41da59476ddcd7b616c2b0f6
alexf <alexf@tut.by>
2024-12-23 18:29:44 +0300

    contacts structure is finished

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from './layout/header/Header';
 import { Main } from './layout/sections/main/Main';
 import { Skills } from './layout/sections/skills/Skills';
 import { Projects } from './layout/sections/projects/Projects';
+import { Contacts } from './layout/sections/contacts/Contacts';
 
 
 
@@ -15,6 +16,7 @@ function App() {
             <Main />
             <Skills/>
             <Projects />
+            <Contacts />
         </div>
     );
 }
---
+++ src/layout/sections/contacts/Contacts.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+
+export const Contacts = () => {
+  return (
+    <StyledContacts>
+      <FlexWrapper direction="column">
+        <Title>For any questions please mail me:</Title>
+        <Text>hi@pavanmg.in</Text>
+      </FlexWrapper>
+    </StyledContacts>
+  );
+};
+
+const StyledContacts = styled.section`
+  background-color: #eca381;
+`;
+
+const Title = styled.h2`
+text-align: center;
+
+`;
+const Text = styled.h5`
+text-align: center;
+`
