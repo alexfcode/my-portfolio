c4b0b05fcc4c8abfcf4ebafb026a23b5af0a5622
alexf <alexf@tut.by>
2025-01-21 11:48:28 +0300

    footer is refactored

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,94 +1,38 @@
-import styled from "styled-components";
 import { FlexWrapper } from "../../components/FlexWrapper";
 import { SocialLinks } from "../../components/socialLinks/SocialLinks";
 import { DesktopMenu } from "../header/menu/DesktopMenu";
 import { Container } from "../../components/Container";
 import { FooterLogo } from "../../components/logo/FooterLogo";
-import { theme } from "../../styles/Theme";
+import { S } from "./Footer_Styled";
 
 export const Footer = () => {
   return (
-    <StyledFooter>
+    <S.Footer>
       <Container>
         <FlexWrapper direction="column" gap="25px">
-          <TopFooterWrapper>
+          <S.TopFooterWrapper>
             <FooterLogo />
-            <FlexWrapper justify="space-between" align="center" wrap="wrap" gap="50px">
-              <PhoneNumber>+91 12345 09876</PhoneNumber>
-              <Email>hi@pavanmg.in</Email>
+            <FlexWrapper
+              justify="space-between"
+              align="center"
+              wrap="wrap"
+              gap="50px"
+            >
+              <S.PhoneNumber>+91 12345 09876</S.PhoneNumber>
+              <S.Email>hi@pavanmg.in</S.Email>
               <SocialLinks />
             </FlexWrapper>
-          </TopFooterWrapper>
+          </S.TopFooterWrapper>
           <hr />
-          <BottomFooterWrapper>
-              <DesktopMenu />
-              <DesignedBy>
-                <span>Designed and built by </span>Pavan MG <span>with</span>{" "}
-                Love <span>&</span> Coffee
-              </DesignedBy>
-          </BottomFooterWrapper>
+          <S.BottomFooterWrapper>
+            <DesktopMenu />
+            <S.DesignedBy>
+              <span>Designed and built by </span>Pavan MG <span>with</span> Love{" "}
+              <span>&</span> Coffee
+            </S.DesignedBy>
+          </S.BottomFooterWrapper>
         </FlexWrapper>
       </Container>
-    </StyledFooter>
+    </S.Footer>
   );
 };
-
-const StyledFooter = styled.footer`
-  padding-bottom: 60px;
-  font-family: "DM Sans";
-  font-size: 18px;
-  font-weight: 400;
-  line-height: 26px;
-`;
-
-const TopFooterWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  @media ${theme.media.mobile} {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-`;
-
-const BottomFooterWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 10px;
-`;
-
-
-const PhoneNumber = styled.span`
-  display: block;
-`;
-
-const Email = styled.span`
-  display: block;
-
-  font-family: "DM Sans";
-
-  background: var(
-    --Gradient-Heading,
-    linear-gradient(90deg, #13b0f5 -2.06%, #e70faa 100%)
-  );
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-`;
-
-const DesignedBy = styled.span`
-  display: block;
-  background: var(
-    --Gradient-Heading,
-    linear-gradient(90deg, #13b0f5 -2.06%, #e70faa 100%)
-  );
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-  font-size: 18px;
-  span {
-    -webkit-text-fill-color: ${theme.colors.primeFont};
-  }
-`;
---
+++ src/layout/footer/Footer_Styled.ts
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+import { theme } from "../../styles/Theme";
+
+const Footer = styled.footer`
+  padding-bottom: 60px;
+  font-family: "DM Sans";
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 26px;
+`;
+
+const TopFooterWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media ${theme.media.mobile} {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const BottomFooterWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 10px;
+`;
+
+const PhoneNumber = styled.span`
+  display: block;
+`;
+
+const Email = styled.span`
+  display: block;
+
+  font-family: "DM Sans";
+
+  background: var(
+    --Gradient-Heading,
+    linear-gradient(90deg, #13b0f5 -2.06%, #e70faa 100%)
+  );
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const DesignedBy = styled.span`
+  display: block;
+  background: var(
+    --Gradient-Heading,
+    linear-gradient(90deg, #13b0f5 -2.06%, #e70faa 100%)
+  );
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 18px;
+  span {
+    -webkit-text-fill-color: ${theme.colors.primeFont};
+  }
+`;
+
+export const S = {
+  Footer,
+  TopFooterWrapper,
+  BottomFooterWrapper,
+  PhoneNumber,
+  Email,
+  DesignedBy,
+};
